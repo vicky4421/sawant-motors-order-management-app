a4dd5cf0125fe1f5f1f59740b7c6d972fc548e20
Vivek Sawant <vicky210490@gmail.com>
2024-06-08 14:14:13 +0530

    init

---
+++ src/main/java/com/vismijatech/main/SawantMotorsOrderManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.vismijatech.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SawantMotorsOrderManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SawantMotorsOrderManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vismijatech/main/SawantMotorsOrderManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vismijatech.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SawantMotorsOrderManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
