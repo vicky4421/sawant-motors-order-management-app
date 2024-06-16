12de6ae3bc2b25b2815513db8963d0bcbb80112b
Vivek Sawant <vicky210490@gmail.com>
2024-06-17 00:19:17 +0530

    define controller classes.

---
+++ src/main/java/com/vismijatech/main/controller/PartyController.java
@@ -0,0 +1,9 @@
+package com.vismijatech.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/party")
+public class PartyController {
+}
