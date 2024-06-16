ea4ed4e84e8c67eecef811eceb9eba716c6c9455
Vivek Sawant <vicky210490@gmail.com>
2024-06-16 23:06:07 +0530

    define bidirectional relation between product and order details.

--- src/main/java/com/vismijatech/main/entity/OrderDetails.java
+++ src/main/java/com/vismijatech/main/entity/OrderDetails.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,8 +24,30 @@ public class OrderDetails {
     @JsonIgnore
     private Order order;
 
+    // bidirectional relation between product and order details
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.DETACH,
+                    CascadeType.REFRESH
+            }
+    )
+    @JoinTable(
+            name = "order_details_product",
+            joinColumns = @JoinColumn(name = "order_details_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> productList;
+
     // constructors
     public OrderDetails(int quantity) {
         this.quantity = quantity;
+    }
+
+    // convenience methods to add products
+    public void addProduct(Product product) {
+        if (productList == null) productList = new ArrayList<>();
+        productList.add(product);
     }
 }
--- src/main/java/com/vismijatech/main/entity/Product.java
+++ src/main/java/com/vismijatech/main/entity/Product.java
@@ -1,12 +1,12 @@
 package com.vismijatech.main.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,9 +19,30 @@ public class Product {
     private String name;
     private String partNumber;
 
+    @ManyToMany(
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.DETACH,
+                    CascadeType.REFRESH
+            }
+    )
+    @JoinTable(
+            name = "order_details_product",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "order_details_id")
+    )
+    private List<OrderDetails> orderDetailsList;
+
     // constructors
     public Product(String name, String partNumber) {
         this.name = name;
         this.partNumber = partNumber;
+    }
+
+    // convenience methods to add order details
+    public void addOrderDetails(OrderDetails orderDetails) {
+        if (orderDetailsList == null) orderDetailsList = new ArrayList<>();
+        orderDetailsList.add(orderDetails);
     }
 }
