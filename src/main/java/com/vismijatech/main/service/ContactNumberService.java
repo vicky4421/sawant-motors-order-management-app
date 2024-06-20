c0f80de2a4de0d840e08befbbc3546d0cb87d7db
Vivek Sawant <vicky210490@gmail.com>
2024-06-20 14:58:55 +0530

    change return type to Optional

--- src/main/java/com/vismijatech/main/service/ContactNumberService.java
+++ src/main/java/com/vismijatech/main/service/ContactNumberService.java
@@ -1,4 +1,10 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.ContactNumber;
+
+import java.util.Optional;
+
 public interface ContactNumberService {
+
+    Optional<ContactNumber> saveContactNumber(ContactNumber contactNumber);
 }
