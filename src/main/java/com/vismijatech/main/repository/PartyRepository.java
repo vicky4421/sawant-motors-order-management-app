94cc925aed6816e80c71594176ed0ab0bda4ae83
Vivek Sawant <vicky210490@gmail.com>
2024-06-20 14:59:53 +0530

    refactor party to supplier

--- src/main/java/com/vismijatech/main/repository/PartyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vismijatech.main.repository;
-
-import com.vismijatech.main.entity.Party;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartyRepository extends JpaRepository<Party, Long> {
-}
--- src/main/java/com/vismijatech/main/service/PartyServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.vismijatech.main.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PartyServiceImpl implements PartyService {
-}
