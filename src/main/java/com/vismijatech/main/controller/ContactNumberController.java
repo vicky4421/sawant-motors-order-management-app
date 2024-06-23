a7030409b862a9614afc6745c5b2fe964090b736
Vivek Sawant <vicky210490@gmail.com>
2024-06-23 22:15:01 +0530

    remove separate entity contact number

--- src/main/java/com/vismijatech/main/controller/ContactNumberController.java
+++
@@ -1,9 +0,0 @@
-package com.vismijatech.main.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/contactNumber")
-public class ContactNumberController {
-}
--- src/main/java/com/vismijatech/main/repository/ContactNumberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vismijatech.main.repository;
-
-import com.vismijatech.main.entity.ContactNumber;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactNumberRepository extends JpaRepository<ContactNumber, Long> {
-}
