d41458b968aa923b1f1ae3e81dbc2e146c7bc6c7
Vivek Sawant <vicky210490@gmail.com>
2024-06-23 22:16:39 +0530

    add delete a supplier method

--- src/main/java/com/vismijatech/main/service/SupplierService.java
+++ src/main/java/com/vismijatech/main/service/SupplierService.java
@@ -2,8 +2,11 @@ package com.vismijatech.main.service;
 
 import com.vismijatech.main.entity.Supplier;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SupplierService {
     Optional<Supplier> saveSupplier(Supplier supplier);
+    Optional<List<Supplier>> getAllSuppliers();
+    Optional<Supplier> deleteSupplierById(Long id);
 }
