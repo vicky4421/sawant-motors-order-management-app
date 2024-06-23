3584d81be8e14634b80eaa5e7f054fb1df9d1091
Vivek Sawant <vicky210490@gmail.com>
2024-06-23 22:18:05 +0530

    refactor getSuppliers and add deleteSupplier method.

--- src/main/java/com/vismijatech/main/controller/SupplierController.java
+++ src/main/java/com/vismijatech/main/controller/SupplierController.java
@@ -1,14 +1,16 @@
 package com.vismijatech.main.controller;
 
 import com.vismijatech.main.dto.SupplierDTO;
-import com.vismijatech.main.entity.ContactNumber;
 import com.vismijatech.main.entity.Supplier;
-import com.vismijatech.main.service.ContactNumberService;
 import com.vismijatech.main.service.SupplierService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/supplier")
@@ -16,31 +18,61 @@ import org.springframework.web.bind.annotation.*;
 public class SupplierController {
     // get supplier service and contact number service reference
     private final SupplierService supplierService;
-    private final ContactNumberService contactNumberService;
 
     private final ModelMapper modelMapper;
 
     // constructor
-    public SupplierController(SupplierService supplierService, ContactNumberService contactNumberService, ModelMapper modelMapper) {
+    public SupplierController(SupplierService supplierService, ModelMapper modelMapper) {
         this.supplierService = supplierService;
-        this.contactNumberService = contactNumberService;
         this.modelMapper = modelMapper;
     }
 
     @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping
     public ResponseEntity<?> saveSupplier(@RequestBody SupplierDTO supplierDTO) {
-        Supplier supplier = new Supplier(supplierDTO.getName());
+        Supplier supplier = new Supplier(supplierDTO.getName(), supplierDTO.getWhatsappNumber());
 
         // check if alternate number is not empty
-        if (supplierDTO.getAlternateNumber() != null) supplier.addContactNumber(
-                new ContactNumber(supplierDTO.getAlternateNumber())
-        );
-
-        supplier.addContactNumber(new ContactNumber(supplierDTO.getWhatsappNumber()));
+        if (supplierDTO.getAlternateNumber() != null) supplier.setAlternateNumber(supplierDTO.getAlternateNumber());
 
         return supplierService.saveSupplier(supplier)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> new ResponseEntity("Supplier not saved!", HttpStatus.BAD_REQUEST));
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/allSuppliers")
+    public ResponseEntity<?> getAllSuppliers() {
+        // get all suppliers from database
+        Optional<List<Supplier>> suppliers = supplierService.getAllSuppliers();
+        List<SupplierDTO> supplierDTOList = new ArrayList<>();
+        if (suppliers.isPresent()){
+            List<Supplier> supplierList = suppliers.get();
+            supplierList.forEach(supplier -> {
+                SupplierDTO supplierDTO = modelMapper.map(supplier, SupplierDTO.class);
+                supplierDTOList.add(supplierDTO);
+            });
+        }
+
+        Optional<List<SupplierDTO>> suppliersDTO = Optional.of(supplierDTOList);
+
+        return suppliersDTO
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity("No suppliers found!", HttpStatus.NOT_FOUND));
+
+    }
+
+    @CrossOrigin(origins = "http://localhost:5173")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteSupplier(@PathVariable Long id) {
+        Supplier supplier = supplierService.deleteSupplierById(id).get();
+
+        SupplierDTO supplierDTO = modelMapper.map(supplier, SupplierDTO.class);
+
+        Optional<SupplierDTO> optionalSupplierDTO = Optional.of(supplierDTO);
+
+        return optionalSupplierDTO
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> new ResponseEntity("Supplier not found!", HttpStatus.NOT_FOUND));
     }
 }
