81e87265bc421c8b49bd4363332b4a786b08d7df
Vivek Sawant <vicky210490@gmail.com>
2024-07-01 09:20:16 +0530

    declare find by category name method

--- src/main/java/com/vismijatech/main/repository/ProductCategoryRepository.java
+++ src/main/java/com/vismijatech/main/repository/ProductCategoryRepository.java
@@ -4,4 +4,6 @@ import com.vismijatech.main.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    // find category by name
+    ProductCategory findByCategoryName(String name);
 }
