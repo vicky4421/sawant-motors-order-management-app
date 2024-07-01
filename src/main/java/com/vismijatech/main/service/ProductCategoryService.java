90b267f8e28da9e7819b6bfc8db392b5de9d1602
Vivek Sawant <vicky210490@gmail.com>
2024-07-01 09:20:38 +0530

    declare methods

--- src/main/java/com/vismijatech/main/service/ProductCategoryService.java
+++ src/main/java/com/vismijatech/main/service/ProductCategoryService.java
@@ -1,4 +1,14 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.ProductCategory;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCategoryService {
+    Optional<ProductCategory> findCategoryByName(String name);
+    Optional<ProductCategory> addCategory(ProductCategory category);
+    Optional<List<ProductCategory>> getAllCategories();
+    Optional<ProductCategory> findCategoryById(Long id);
+    Optional<ProductCategory> deleteCategory(Long id);
 }
