a30400cb27a8c13d27c29e4856ae9d9203b0eb58
Vivek Sawant <vicky210490@gmail.com>
2024-07-01 09:19:34 +0530

    refactor variables

--- src/main/java/com/vismijatech/main/dto/ProductCategoryDTO.java
+++ src/main/java/com/vismijatech/main/dto/ProductCategoryDTO.java
@@ -1,13 +1,21 @@
 package com.vismijatech.main.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ProductCategoryDTO {
     private Long id;
-    private String name;
+    private String categoryName;
+    private List<ProductDTO> productList;
+    private String parentCategory;
+    private List<ProductCategoryDTO> childCategories;
+
 }
