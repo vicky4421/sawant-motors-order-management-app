337c26f2f28e8ab9c206b87ed1b5b19ede2e0d9f
Vivek Sawant <vicky210490@gmail.com>
2024-07-01 09:19:08 +0530

    add builder lombok annotation

--- src/main/java/com/vismijatech/main/dto/ProductDTO.java
+++ src/main/java/com/vismijatech/main/dto/ProductDTO.java
@@ -1,12 +1,14 @@
 package com.vismijatech.main.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ProductDTO {
     private Long id;
     private String name;
