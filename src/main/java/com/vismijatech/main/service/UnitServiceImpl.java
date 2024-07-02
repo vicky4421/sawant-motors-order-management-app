c997b90b36d5fa95059ebe5923a5bfbd58fd24b7
Vivek Sawant <vicky210490@gmail.com>
2024-07-02 22:31:45 +0530

    implement methods

--- src/main/java/com/vismijatech/main/service/UnitServiceImpl.java
+++ src/main/java/com/vismijatech/main/service/UnitServiceImpl.java
@@ -1,7 +1,65 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.Unit;
+import com.vismijatech.main.repository.UnitRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UnitServiceImpl implements UnitService {
+    // get repository reference
+    private final UnitRepository unitRepository;
+    // constructor
+
+    public UnitServiceImpl(UnitRepository unitRepository) {
+        this.unitRepository = unitRepository;
+    }
+
+    // save unit
+    @Override
+    public Optional<Unit> addUnit(Unit unit) {
+        // save unit to database
+        Unit unit1 = unitRepository.save(unit);
+        return Optional.of(unit1);
+    }
+
+    // update unit
+    @Override
+    public Optional<Unit> updateUnit(Unit unit) {
+        System.out.println("Unit: " + unit);
+        // find unit in database
+        Optional<Unit> unit1 = unitRepository.findById(unit.getId());
+        if (unit1.isPresent()) {
+            // update unit
+            Unit unit2 = unitRepository.save(unit);
+            return Optional.of(unit2);
+        }
+        return Optional.empty();
+    }
+
+    // get all units
+    @Override
+    public Optional<List<Unit>> getAllUnits() {
+        List<Unit> units = unitRepository.findAll();
+        if (!units.isEmpty()) {
+            return Optional.of(units);
+        }
+        return Optional.empty();
+    }
+
+    // delete unit
+    @Override
+    public Optional<Unit> deleteUnit(Long id) {
+        // find unit in database
+        Optional<Unit> unit1 = unitRepository.findById(id);
+        if (unit1.isPresent()) {
+            // delete unit
+            unitRepository.deleteById(id);
+            return unit1;
+        }
+        return Optional.empty();
+    }
+
 }
