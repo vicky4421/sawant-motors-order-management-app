0fc98d930b61d81270b444e607f666530ac2e2a1
Vivek Sawant <vicky210490@gmail.com>
2024-07-02 22:31:36 +0530

    declare methods

--- src/main/java/com/vismijatech/main/service/UnitService.java
+++ src/main/java/com/vismijatech/main/service/UnitService.java
@@ -1,4 +1,13 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.Unit;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UnitService {
+    public Optional<Unit> addUnit(Unit unit);
+    public Optional<Unit> updateUnit(Unit unit);
+    public Optional<List<Unit>> getAllUnits();
+    public Optional<Unit> deleteUnit(Long id);
 }
