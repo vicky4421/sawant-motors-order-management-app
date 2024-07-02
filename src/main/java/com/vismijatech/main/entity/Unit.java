05a519a76f90b24615d763c02a5eeb3bed5480e9
Vivek Sawant <vicky210490@gmail.com>
2024-07-02 22:30:58 +0530

    add lombok builder annotation

--- src/main/java/com/vismijatech/main/entity/Unit.java
+++ src/main/java/com/vismijatech/main/entity/Unit.java
@@ -2,6 +2,7 @@ package com.vismijatech.main.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Entity
 public class Unit {
     @Id
