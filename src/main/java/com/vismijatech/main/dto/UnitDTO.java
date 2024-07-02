5dc8b266e04474956cffbb8359a7f007e126978d
Vivek Sawant <vicky210490@gmail.com>
2024-07-02 22:31:19 +0530

    add lombok builder annotation

--- src/main/java/com/vismijatech/main/dto/UnitDTO.java
+++ src/main/java/com/vismijatech/main/dto/UnitDTO.java
@@ -1,12 +1,14 @@
 package com.vismijatech.main.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UnitDTO {
     private Long id;
     private String name;
