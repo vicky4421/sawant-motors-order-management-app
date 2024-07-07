6bf9699f8e7fdc2c7f676255865787c9f2393351
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:05:29 +0530

    implement order services.

--- src/main/java/com/vismijatech/main/service/OrderServiceImpl.java
+++ src/main/java/com/vismijatech/main/service/OrderServiceImpl.java
@@ -1,7 +1,44 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.Order;
+import com.vismijatech.main.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    // get order repository reference
+    private final OrderRepository orderRepository;
+
+    // constructor
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+    @Override
+    public Optional<Order> saveOrder(Order order) {
+        return Optional.of(orderRepository.save(order));
+    }
+
+    @Override
+    public List<Optional<Order>> getAllOrders() {
+        return orderRepository.findAll().stream().map(Optional::of).toList();
+    }
+
+    @Override
+    public Optional<Order> findOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Order> updateOrder(Order order) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Order> deleteOrder(Long id) {
+        return Optional.empty();
+    }
 }
