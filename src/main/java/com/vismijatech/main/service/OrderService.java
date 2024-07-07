ccb6ef1c30915bbae999dfc7c5b740d7bee1bd10
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:05:19 +0530

    define order services.

--- src/main/java/com/vismijatech/main/service/OrderService.java
+++ src/main/java/com/vismijatech/main/service/OrderService.java
@@ -1,4 +1,14 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.Order;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface OrderService {
+    public Optional<Order> saveOrder(Order order);
+    public List<Optional<Order>> getAllOrders();
+    public Optional<Order> findOrderById(Long id);
+    public Optional<Order> updateOrder(Order order);
+    public Optional<Order> deleteOrder(Long id);
 }
