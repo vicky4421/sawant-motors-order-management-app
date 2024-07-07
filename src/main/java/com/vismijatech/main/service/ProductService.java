a89c421a1e30110e46b289bac131e4ba08e24b4d
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 15:04:38 +0530

    define methods for products service

--- src/main/java/com/vismijatech/main/service/ProductService.java
+++ src/main/java/com/vismijatech/main/service/ProductService.java
@@ -1,4 +1,14 @@
 package com.vismijatech.main.service;
 
+import com.vismijatech.main.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductService {
+    public Optional<Product> addProduct(Product product);
+    public Optional<Product> updateProduct(Product product);
+    public Optional<Product> deleteProduct(Long id);
+    public Optional<List<Product>> getAllProducts();
+    public Optional<Product> getProductById(Long id);
 }
