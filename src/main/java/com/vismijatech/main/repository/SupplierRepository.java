4ff9de6e823046eae193748ede8dc7174287a2fc
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:02:34 +0530

    define find by name

--- src/main/java/com/vismijatech/main/repository/SupplierRepository.java
+++ src/main/java/com/vismijatech/main/repository/SupplierRepository.java
@@ -4,4 +4,5 @@ import com.vismijatech.main.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
+    Supplier findByName(String name);
 }
