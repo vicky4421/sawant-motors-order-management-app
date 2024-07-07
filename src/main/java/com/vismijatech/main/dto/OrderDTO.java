97ece4241250d7eed05f278a71942e03324af618
Vivek Sawant <vicky210490@gmail.com>
2024-07-07 22:05:03 +0530

    refactor variables.

--- src/main/java/com/vismijatech/main/dto/OrderDTO.java
+++ src/main/java/com/vismijatech/main/dto/OrderDTO.java
@@ -1,16 +1,16 @@
 package com.vismijatech.main.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class OrderDTO {
     private Long id;
     private String name;
-    private LocalDate orderDate;
+    private String supplier;
 }
