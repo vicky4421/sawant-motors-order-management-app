987d9e362ff7ed9329b96bd5bb573fcca3593d45
Vivek Sawant <vicky210490@gmail.com>
2024-07-17 18:42:05 +0530

    declare get all orders method.

--- src/main/java/com/vismijatech/main/service/OrderService.java
+++ src/main/java/com/vismijatech/main/service/OrderService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface OrderService {
     public Optional<Order> saveOrder(Order order);
-    public List<Optional<Order>> getAllOrders();
+    public Optional<List<Order>> getAllOrders();
     public Optional<Order> findOrderById(Long id);
     public Optional<Order> updateOrder(Order order);
     public Optional<Order> deleteOrder(Long id);
