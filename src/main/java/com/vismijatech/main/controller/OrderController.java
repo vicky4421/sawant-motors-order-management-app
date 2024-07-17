8e3154ae6ad27b49eb062d7c6c9e6c6806bd4acd
Vivek Sawant <vicky210490@gmail.com>
2024-07-17 18:41:18 +0530

    define get all orders endpoint

--- src/main/java/com/vismijatech/main/controller/OrderController.java
+++ src/main/java/com/vismijatech/main/controller/OrderController.java
@@ -5,16 +5,16 @@ import com.vismijatech.main.entity.Order;
 import com.vismijatech.main.entity.Supplier;
 import com.vismijatech.main.service.OrderService;
 import com.vismijatech.main.service.SupplierService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/order")
@@ -23,11 +23,13 @@ public class OrderController {
     // get order service reference
     private final OrderService orderService;
     private final SupplierService supplierService;
+    private final ModelMapper modelMapper;
 
     // constructor
-    public OrderController(OrderService orderService, SupplierService supplierService) {
+    public OrderController(OrderService orderService, SupplierService supplierService, ModelMapper modelMapper) {
         this.orderService = orderService;
         this.supplierService = supplierService;
+        this.modelMapper = modelMapper;
     }
 
     // save order
@@ -37,8 +39,8 @@ public class OrderController {
                 .name(orderDTO.getName())
                 .build();
 
-        if (orderDTO.getSupplier() != null){
-            Optional<Supplier> supplier = supplierService.findSupplierByName(orderDTO.getSupplier());
+        if (orderDTO.getSupplierName() != null){
+            Optional<Supplier> supplier = supplierService.findSupplierByName(orderDTO.getSupplierName());
             supplier.ifPresent(newOrder::setSupplier);
         } else {
             return new ResponseEntity("Supplier not found!", HttpStatus.BAD_REQUEST);
@@ -54,5 +56,28 @@ public class OrderController {
         return orderService.saveOrder(newOrder)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> new ResponseEntity("Order not saved!", HttpStatus.BAD_REQUEST));
+    }
+
+    // get all orders
+    @CrossOrigin(origins = "http://localhost:5173")
+    @GetMapping("/allOrders")
+    public ResponseEntity<?> getAllOrders(){
+        Optional<List<Order>> orders = orderService.getAllOrders();
+        if (orders.isPresent()){
+            List<Order> orderList = orders.get();
+            List<OrderDTO> orderDTOList = orderList.stream()
+                    .map(
+                            order -> {
+                                return modelMapper.map(order, OrderDTO.class);
+                            }
+                    )
+                    .collect(Collectors.toList());
+            Optional<List<OrderDTO>> orderDTOS = Optional.of(orderDTOList);
+            return orderDTOS
+                    .map(ResponseEntity::ok)
+                    .orElseGet(() -> new ResponseEntity("No orders found", HttpStatus.NOT_FOUND));
+        } else {
+            return new ResponseEntity<>("No orders found", HttpStatus.NOT_FOUND);
+        }
     }
 }
